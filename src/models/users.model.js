ba74af2724ae384e0a355ae53ac65381692fcbcc
Aniruddha1609 <anniruddha2003@gmail.com>
2026-07-27 21:50:59 +0530

    feat: implement user model with authentication methods and token generation

---
+++ src/models/users.model.js
@@ -0,0 +1,72 @@
+import mongoose, { mongo, Schema } from "mongoose";
+import bcrypt from "bcrypt";
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            lowercase: true,
+            trim: true,
+            unique: true,
+            index: true,
+        },
+
+        fullname: {
+            type: String,
+            required: true,
+            index: true,
+        },
+
+        avatar: {
+            type: String,
+        },
+
+        password: {
+            type: String,
+            required: true,
+        },
+
+        refreshtoken: {
+            type: String,
+        },
+    },
+    {
+        timestamps: true,
+    },
+);
+
+userSchema.pre("save", async () => {
+    if (!this.isModified("password")) return;
+    this.password = await bcrypt(this.password, 10);
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateRefreshToken = async function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: REFRESH_TOKEN_EXPIRY,
+        },
+    );
+};
+
+userSchema.methods.generateAccessToken = async function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.Access_TOKEN_SECRET,
+        {
+            expiresIn: Access_TOKEN_EXPIRY,
+        },
+    );
+};
+
+export const User = mongoose.model("User", userSchema);
